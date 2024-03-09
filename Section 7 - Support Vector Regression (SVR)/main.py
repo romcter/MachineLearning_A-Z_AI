13b3dd6fd181c12f4dcf531fc9a378de54fc9e44
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-09 12:43:11 +0200

    Section 7 - Support Vector Regression (SVR)

--- Section 7 - Support Vector Regression (SVR)/main.py
+++ Section 7 - Support Vector Regression (SVR)/main.py
@@ -1,6 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.svm import SVR
 
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -18,9 +20,25 @@ regressor = SVR(kernel='rbf')
 regressor.fit(X,y)
 sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1,1))
 
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
-plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1,1)), color='blue')
+x = sc_X.inverse_transform(X)
+Y = sc_y.inverse_transform(y)
+
+plt.scatter(x, Y, color='red')
+plt.plot(x, sc_y.inverse_transform(regressor.predict(X).reshape(-1,1)), color='blue')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
+
+lin_reg = LinearRegression()
+poly_reg = PolynomialFeatures(degree=4)
+
+X_grid = np.arange(min(x), max(x), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(x, Y, color='red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid)).reshape(-1,1)), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
