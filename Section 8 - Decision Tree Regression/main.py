2f205775b2890de8e5d6277a1dd635cb530b30af
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-09 13:29:32 +0200

    Section 8 - Decision Tree Regression

--- Section 8 - Decision Tree Regression/main.py
+++ Section 8 - Decision Tree Regression/main.py
@@ -13,7 +13,7 @@ regressor.fit(X, y)
 
 print("Predicted : % d\n" % regressor.predict([[6.5]]))
 
-X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
 plt.plot(X_grid, regressor.predict(X_grid), color='blue')
