c0e17ef4c22203e83aa43ff5cdc5fbfb978a1f30
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-09 13:27:39 +0200

    Section 8 - Decision Tree Regression

---
+++ Section 8 - Decision Tree Regression/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+regressor = DecisionTreeRegressor(random_state=0)
+
+regressor.fit(X, y)
+
+print("Predicted : % d\n" % regressor.predict([[6.5]]))
+
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
