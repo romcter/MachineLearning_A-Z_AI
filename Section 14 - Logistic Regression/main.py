8d9d072e581260f163e8e72b9078d3aa49b824f7
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-14 11:03:17 +0200

    Section 14 - Logistic Regression

--- Section 14 - Logistic Regression/main.py
+++ Section 14 - Logistic Regression/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -8,9 +9,33 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+# np.set_printoptions(precision=2)
+# print(y_test.reshape(len(y_test), 1))
+# print("=============================")
+# print(y_pred.reshape(len(y_pred), 1))
+# print("=============================")
+# print(classifier.predict(scaler.transform([[30,87000]])))
+print(np.concatenate((y_test.reshape(len(y_test), 1), y_pred.reshape(len(y_pred), 1)), 1))
+
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+cm_display = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
+accuracy_score(y_test, y_pred)
+
+cm_display.plot()
+plt.show()
 
 # X_grid = np.arange(min(X), max(X), 0.01)
 # X_grid = X_grid.reshape((len(X_grid), 1))
