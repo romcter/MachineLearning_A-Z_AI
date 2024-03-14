c7ffe776900ac7c7aeafb36893282f0f182d9a88
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-14 11:36:53 +0200

    Section 14 - Logistic Regression

--- Section 14 - Logistic Regression/main.py
+++ Section 14 - Logistic Regression/main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
 
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
-from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
@@ -16,16 +17,10 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-from sklearn.linear_model import LogisticRegression
-
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 # np.set_printoptions(precision=2)
-# print(y_test.reshape(len(y_test), 1))
-# print("=============================")
-# print(y_pred.reshape(len(y_pred), 1))
-# print("=============================")
 # print(classifier.predict(scaler.transform([[30,87000]])))
 print(np.concatenate((y_test.reshape(len(y_test), 1), y_pred.reshape(len(y_pred), 1)), 1))
 
@@ -34,14 +29,32 @@ print(confusion_matrix)
 cm_display = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=[False, True])
 accuracy_score(y_test, y_pred)
 
-cm_display.plot()
+X_set, y_set = scaler.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.25))
+plt.contourf(X1, X2, classifier.predict(scaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('salmon', 'dodgerblue'))(i), label=j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
 
-# X_grid = np.arange(min(X), max(X), 0.01)
-# X_grid = X_grid.reshape((len(X_grid), 1))
-# plt.scatter(X, y, color='red')
-# plt.plot(X_grid, regressor.predict(X_grid), color='blue')
-# plt.title('Truth or Bluff (Random Forest Regressor)')
-# plt.xlabel('Position level')
-# plt.ylabel('Salary')
-# plt.show()
+X_set, y_set = scaler.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.25),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.25))
+plt.contourf(X1, X2, classifier.predict(scaler.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('salmon', 'dodgerblue'))(i), label=j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
