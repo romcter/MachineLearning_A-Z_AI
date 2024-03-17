f8a5795098551be0a968cc2fe2bc7505e2393e59
rpryimuk <roman.pryimuk@capgemini.com>
2024-03-17 10:16:30 +0200

    Section 20 - Random Forest Classification

--- Section 20 - Random Forest Classifier/main.py
+++ Section 20 - Random Forest Classifier/main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import StandardScaler
 from matplotlib.colors import ListedColormap
-from sklearn.linear_model import LogisticRegression
 
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
@@ -17,7 +17,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-classifier = LogisticRegression(random_state=0)
+classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 # np.set_printoptions(precision=2)
@@ -38,7 +38,7 @@ plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('salmon', 'dodgerblue'))(i), label=j)
-plt.title('Logistic Regression (Training set)')
+plt.title('Random Forest Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -53,7 +53,7 @@ plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('salmon', 'dodgerblue'))(i), label=j)
-plt.title('Logistic Regression (Test set)')
+plt.title('Random Forest Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
